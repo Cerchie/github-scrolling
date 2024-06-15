90b4990a4d16e42d6035edb58a095f256e4d98be
Cerchie <luciacerchie@gmail.com>
2024-06-15 15:45:15 -0700

    working on pie

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ console.log(languages)
 // set the dimensions and margins of the graph
 var width = 400
 var height = 400
-var margin = 40
+var margin = 0
 
     // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
 var radius = Math.min(width, height) / 2 - margin
@@ -41,16 +41,15 @@ var svg = d3.select("svg")
 
 // get data
 var data = languages;
-console.log(data)
-
-var min = Math.min(...Object.values(data))
-var max = Math.max(...Object.values(data))
 
 // set the color scale
+// Define a color scale
 var color = d3.scaleOrdinal()
-  .domain([min, max])
-  .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+  .range(d3.schemeSet2); // Use a D3 color scheme for the range
 
+
+
+  color.domain(Object.keys(data));
 // Compute the position of each group on the pie:
 var pie = d3.pie()
   .value(function(d) {console.log('d val', d); return d[1]; })
@@ -85,12 +84,8 @@ svg.selectAll('text')
 .enter()
 .append('text')
 .text(function(d) { return d.data[0]})
-.attr('transform', function(d) {
-  var pos = arc.centroid(d);
-  return 'translate(' + pos + ')';
-})
-.style('text-anchor', 'middle')
-.style('font-size', '12px');
+.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")";  })
+.style("font-size", 17)
 
 //end language function
 }
